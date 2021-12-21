0d40035b0760c6be6f36029919301a2347c78090
Abdullaev-kosimzho-002 <dzukosta348@gmail.com>
2021-12-21 15:05:24 +0300

    Metanit extra lesson

---
+++ Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Metanit_допольнительный_курс
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+                 //Упражнение 1.
+
+            // int number_1;
+            // int number_2;
+            // Console.WriteLine("Два числа: ");
+            // number_1 = Convert.ToInt32(Console.ReadLine());
+            // number_2 = Convert.ToInt32(Console.ReadLine());
+            // if (number_1 > number_2 || number_1 < number_2)
+            // {
+            //     Console.WriteLine($"Результат сравнения: {number_1 == number_2}");
+            // }
+            
+            //Упражнение 2.
+
+            // int number = Convert.ToInt32(Console.ReadLine());
+            // Console.WriteLine(number);
+            // if (number > 5 && number < 10)
+            // {
+            //     Console.WriteLine("Число больше 5 и меньше 10");
+            // }
+            // else
+            // {
+            //     Console.WriteLine("Неизвестное число");
+            // }
+            //
+
+
+         //Упражнения 3.
+
+         // int number_z = Convert.ToInt32(Console.ReadLine());
+         // Console.WriteLine(number_z);
+         // if (number_z == 5 || number_z == 10)
+         // {
+         //     Console.WriteLine("Либо равно 5,либо равно 10");
+         // }
+         // else
+         // {
+         //     Console.WriteLine("Неизвестное число");
+         // }
+
+        //Упражнение 4.
+
+        // double summ_1 = Convert.ToDouble(Console.ReadLine());
+        // double summ_2 = Convert.ToDouble(Console.ReadLine());
+        // Console.WriteLine(summ_1);
+        // Console.WriteLine(summ_2);
+        // double result = summ_1 + summ_2;
+        // if (result < 100 )
+        // {
+        //  Console.WriteLine("Начисляется 5%");   
+        // }else if (result == 130)
+        // {
+        //     Console.WriteLine("Начисляется 7%");
+        // }else if (result > 200)
+        // {
+        //     Console.WriteLine("Начисляется 10%");
+        // }
+
+        
+        //Упражнение 6.
+        
+        // int numeral = Convert.ToInt32(Console.ReadLine());
+        // Console.WriteLine(numeral);
+        // switch (numeral)
+        // {
+        //     case 1:
+        //         Console.WriteLine("Сложение");
+        //         break;
+        //     case 2:
+        //         Console.WriteLine("Вычитание");break;
+        //     case 3:
+        //         Console.WriteLine("Умножение");break;
+        //     default:Console.WriteLine("Операция неопределена");
+        //         break;
+        //     
+        //
+        // }
+
+        }
+    }
+}
